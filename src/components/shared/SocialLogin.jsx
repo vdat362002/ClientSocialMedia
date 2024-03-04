9961347b84b1148c306a1c2fe1425cff91ab8445
vovanhung-dev <67744712+vovanhung-dev@users.noreply.github.com>
2024-03-05 00:32:43 +0700

    update task 1

--- src/components/shared/SocialLogin.jsx
+++ src/components/shared/SocialLogin.jsx
@@ -7,14 +7,14 @@ const SocialLogin = ({ isLoading }) => {
 
     return (
         <div className="w-full flex space-x-2 items-center">
-            <a
+            {/* <a
                 className="button w-full bg-blue-500 hover:bg-blue-600"
                 href={`${process.env.REACT_APP_SocialMedia_URL}/api/v1/auth/facebook`}
                 onClick={onClickSocialLogin}
             >
                 <FacebookFilled className="m-0 laptop:mr-4 text-xl laptop:text-sm" />
                 <span className="hidden laptop:inline-block">Facebook</span>
-            </a>
+            </a> */}
             <a
                 className="button w-full text-gray-800 bg-white hover:bg-gray-200 border border-gray-200"
                 href={`${process.env.REACT_APP_SocialMedia_URL}/api/v1/auth/google`}
